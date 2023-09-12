bfc629f1be55aca211e8edfd5b22689f7f039194
NiChrosia <nichrosian@gmail.com>
2023-09-11 20:17:38 -0500

    feat: implement cli processor

---
+++ src/cli.nim
@@ -0,0 +1,58 @@
+import log
+import std/[tables, sets, sugar, strformat]
+
+type
+    Cmd = (arguments: seq[string], options: Table[string, string], flags: HashSet[string]) -> void
+
+    CommandProcessor* = object
+        helpEntries: Table[string, string]
+        options: HashSet[string]
+
+        default: Cmd
+        commands: Table[string, Cmd]
+
+proc init*(
+    _: typedesc[CommandProcessor], 
+    entries: Table[string, string], 
+    options: HashSet[string], 
+    default: Cmd, 
+    commands: Table[string, Cmd]
+): CommandProcessor =
+    result.helpEntries = entries
+    result.default = default
+    result.commands = commands
+    result.options = options
+
+proc process*(processor: CommandProcessor, rawArguments: seq[string]) =
+    if rawArguments.len == 0:
+        processor.default(@[], initTable[string, string](), initHashSet[string]())
+
+    let commandName = rawArguments[0]
+
+    if commandName notin processor.commands:
+        log.error(fmt"no such command '{commandName}'!")
+        return
+
+    var arguments: seq[string]
+    var options: Table[string, string]
+    var flags: HashSet[string]
+
+    var optionKey = ""
+
+    for rawArgument in rawArguments:
+        if optionKey != "":
+            options[optionKey] = rawArgument
+            continue
+
+        if rawArgument in processor.options:
+            optionKey = rawArgument
+            continue
+
+        if rawArgument[0] == '-':
+            flags.incl(rawArgument)
+            continue
+
+        arguments.add(rawArgument)
+
+    let command = processor.commands[commandName]
+    command(arguments, options, flags)
