e0bdb164c355e1294a36501be8d05d0f17c7a6d6
NiChrosia <nichrosian@gmail.com>
2023-09-11 20:08:57 -0500

    feat: add logfile
    feat: display time in log messages

--- src/log.nim
+++ src/log.nim
@@ -1,24 +1,49 @@
-import std/[terminal]
+import std/[terminal, times, strformat]
 
-const INFO_COLOR = fgBlue
-const DEBUG_COLOR = fgGreen
-const ERROR_COLOR = fgRed
+const
+    INFO_COLOR = fgBlue
+    DEBUG_COLOR = fgGreen
+    ERROR_COLOR = fgRed
 
-proc writeValues(values: openArray[string]) =
+    LOG_FILE_NAME = "musyn.log"
+
+var logFile: File
+
+# utility
+proc writeValues(values: openArray[string], file: File) =
+    let hours = now().hour()
+    let minutes = now().minute()
+    let seconds = now().second()
+
+    file.write(fmt"[{hours}:{minutes}:{seconds}]")
+
     for value in values:
-        stdout.write(value)
+        file.write(value)
 
-    stdout.write("\n")
+    file.write("\n")
 
+# api
 proc info*(values: varargs[string, `$`]) =
     stdout.styledWrite(INFO_COLOR, "[I]")
-    writeValues(values)
+
+    writeValues(values, stdout)
+    writeValues(values, logFile)
 
 proc debug*(values: varargs[string, `$`]) =
     stdout.styledWrite(DEBUG_COLOR, "[D]")
-    writeValues(values)
+
+    writeValues(values, stdout)
+    writeValues(values, logFile)
 
 proc error*(values: varargs[string, `$`]) =
     stdout.styledWrite(ERROR_COLOR, "[E]")
-    writeValues(values)
 
+    writeValues(values, stdout)
+    writeValues(values, logFile)
+
+# state
+proc init*() =
+    logFile = open(LOG_FILE_NAME)
+
+proc terminate*() =
+    logFile.close()
